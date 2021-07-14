dfaab7c57e977ee91afd35961e3780cf8762d1f4
DOUGLAS STREY <douglas.strey@bonja.local>
2021-07-14 14:33:50 -0300

    using .show() and .hide()

---
+++ app.js
@@ -0,0 +1,9 @@
+function animationTitle() {
+    $("h1.teste-title").hide().show(1500);
+    $("h2.jquery-title").hide().show(1000);
+    $("img.jquery-logo").hide(500).show(1200);
+
+    return 0;
+}
+
+animationTitle();
